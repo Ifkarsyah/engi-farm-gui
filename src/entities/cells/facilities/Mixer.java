193c5c84e2134f99463e20a9cc76f18d418f6717
AgentNine <juniardiakbar@gmail.com>
2019-04-14 15:52:22 +0700

    Mixer

--- src/entities/cells/facilities/Mixer.java
+++ src/entities/cells/facilities/Mixer.java
@@ -10,16 +10,26 @@ public class Mixer extends Facility {
     // Mix method
     // Param : Reference to player and side product which is going to be produced
 
-    /*
-     * public void mix(List<Product> &inv, SideProduct &sp, bool *success) {
-     * List<Product> prod = sp.getProductList(); success = true;
-     * 
-     * for (int i = 0; i < prod.getSize(); i++) { Product bahan = prod.get(i); if
-     * (inv.find(bahan) == -1) { success = false; break; } }
-     * 
-     * if (*success) { success = true; for (int i = 0; i < prod.getSize(); i++) {
-     * Product bahan = prod.get(i); inv.remove(bahan); } inv.add(sp); } }
-     */
+    public void mix(List<Product> inv, SideProduct sp, boolean success) {
+        List<Product> prod = new List();
+        prod.copy(sp.getProductList());
+        success = true;
+        for (int i = 0; i < prod.getSize(); i++) {
+            Product bahan = prod.get(i);
+            if (inv.find(bahan) == -1){
+                success = false;
+                break;
+            }
+        }
+        if (success) {
+            success = true;
+            for (int i = 0; i < prod.getSize(); i++){
+                Product bahan = prod.get(i);
+                inv.remove(bahan);
+            }
+            inv.add(sp);
+        }
+    }
 
     public char render() {
         return 'M';
